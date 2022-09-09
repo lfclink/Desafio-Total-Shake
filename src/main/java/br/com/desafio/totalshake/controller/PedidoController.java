c0a3b952dc7b68e661cfe3f6c915a42d71076a6e
Luiz Flávio Clink <lfclink@ciandt.com>
2022-09-09 10:08:40 -0300

    Week 3 - 1.1 - create com uma lista de itensPedido no Pedido

--- src/main/java/br/com/desafio/totalshake/controller/PedidoController.java
+++ src/main/java/br/com/desafio/totalshake/controller/PedidoController.java
@@ -2,7 +2,9 @@ package br.com.desafio.totalshake.controller;
 
 import br.com.desafio.totalshake.dto.pedido.PedidoInputDTO;
 import br.com.desafio.totalshake.dto.pedido.PedidoOutputDTO;
+import br.com.desafio.totalshake.model.ItemPedido;
 import br.com.desafio.totalshake.model.Pedido;
+import br.com.desafio.totalshake.repositories.ItemPedidoRepository;
 import br.com.desafio.totalshake.services.PedidoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,6 +22,9 @@ public class PedidoController {
 
     @Autowired
     PedidoService pedidoService;
+
+    @Autowired
+    ItemPedidoRepository itemPedidoRepository;
 
     @Autowired
     ModelMapper modelMapper;
@@ -39,11 +45,19 @@ public class PedidoController {
     @PostMapping
     public ResponseEntity<?> createPedido(@Valid @RequestBody PedidoInputDTO novoPedido){
         Pedido pedido = modelMapper.map(novoPedido, Pedido.class);
+        List<ItemPedido> itensPedido = pedido.getItensPedidoList();
+
         if(pedido.getDataHora() == null){
             pedido.setDataHora(LocalDateTime.now());
         }
 
-        return ResponseEntity.ok(pedidoService.create(pedido));
+        Pedido createdPedido = pedidoService.create(pedido);
+
+        if(!itensPedido.isEmpty()){
+            itemPedidoRepository.saveAll(itensPedido);
+        }
+
+        return ResponseEntity.ok(createdPedido);
     }
 
     @PutMapping(value = "/{id}")
